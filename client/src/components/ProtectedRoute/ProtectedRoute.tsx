5fcc7d8c0b28bff5ab1ff99c486a2613440dc880
labaran1 <adlabaran@gmail.com>
2021-11-21 09:23:23 +0000

    protected route

---
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Redirect, Route, RouteProps } from 'react-router';
+import { useAuth } from '../../context/useAuthContext';
+
+export interface IPrivateRouteProps extends RouteProps {
+  redirectPath: string; // redirect path if don't authenticate route
+}
+
+const PrivateRoute: React.FC<IPrivateRouteProps> = (props) => {
+  const { loggedInUser } = useAuth();
+
+  return loggedInUser?.email ? (
+    <Route {...props} component={props.component} render={undefined} />
+  ) : (
+    <Redirect to={{ pathname: props.redirectPath }} />
+  );
+};
+
+export default PrivateRoute;
