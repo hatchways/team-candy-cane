d17e04e978e7588dd179007d0d741b9664d62ac4
labaran1 <adlabaran@gmail.com>
2021-11-24 12:01:19 +0000

    rename interface

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Redirect, Route, RouteProps } from 'react-router';
 import { useAuth } from '../../context/useAuthContext';
 
-export interface IPrivateRouteProps extends RouteProps {
+export interface PrivateRouteProps extends RouteProps {
   redirectPath: string; // redirect path if don't authenticate route
 }
 
-const PrivateRoute: React.FC<IPrivateRouteProps> = (props) => {
+const PrivateRoute: React.FC<PrivateRouteProps> = (props) => {
   const { loggedInUser } = useAuth();
 
   return loggedInUser?.email ? <Route {...props} component={props.component} /> : <Redirect to="/login" />;
